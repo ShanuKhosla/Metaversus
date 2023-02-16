a1c289d963c268eb70d14d3fd5056d83f881a5e3
Shantanu <shantanukhoslas@gmail.com>
2023-02-16 17:10:09 +0530

    changed brackets in newFeatures.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,20 +11,20 @@ const Navbar = () => (
     initial="hidden"
     whileInView="show"
     className={`${styles.xPadding} py-8 relative`}
-    >
-      <div className="absolute w-[50%] inset-0 gradient-01" />
-        <div className={`${styles.innerWidth} mx-auto flex justify-between`}>
-          <img src="/search.png"
-            alt="search"
-            className=" w-[24px] h-[24px] object-contain"
-            />
-          <h2 className="text-[24px] font-extrabold leading-[30px] text-white">METAVERSUS</h2>
-          <img
-            src="/menu.png"
-            alt="hamBurger Menu"
-            className="w-[24px] h-[24px] object-contain "
-             />
-        </div>
+  >
+    <div className="absolute w-[50%] inset-0 gradient-01" />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between`}>
+      <img src="/search.png"
+        alt="search"
+        className=" w-[24px] h-[24px] object-contain"
+      />
+      <h2 className="text-[24px] font-extrabold leading-[30px] text-white">METAVERSUS</h2>
+      <img
+        src="/menu.png"
+        alt="hamBurger Menu"
+        className="w-[24px] h-[24px] object-contain "
+      />
+    </div>
   </motion.nav>
 );
 
